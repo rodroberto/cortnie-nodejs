bedb40bbe2d4a92f65dba417bbdb1ae11fe2b004
rodroberto <roberto.rodriguez.dev1125@gmail.com>
2024-07-27 19:18:08 -0300

    Fixed CORS error

--- functions/index.js
+++ functions/index.js
@@ -2,43 +2,79 @@ const functions = require('firebase-functions');
 const admin = require('firebase-admin');
 admin.initializeApp();
 
-const db = admin.firestore();
+exports.addObject = functions.https.onRequest((req, res) => {
+  res.set('Access-Control-Allow-Origin', '*');
+  res.set('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  res.set('Access-Control-Allow-Headers', 'Content-Type');
 
-exports.addObject = functions.https.onRequest(async (req, res) => {
+  // Respond to preflight requests
+  if (req.method === 'OPTIONS') {
+    res.status(204).send('');
+    return;
+  }
+
   const data = req.body;
-  if (!data.obj_id) {
-    return res.status(400).send('obj_id is required');
-  }
-  await db.collection('objects').doc(data.obj_id).set(data);
-  res.status(200).send('Object added');
+  admin.firestore().collection('objects').doc(data.obj_id).set(data)
+    .then(() => {
+      res.status(200).send('Object added/updated successfully');
+    })
+    .catch((error) => {
+      console.error('Error adding/updating object:', error);
+      res.status(500).send('Error adding/updating object');
+    });
 });
 
-exports.getObject = functions.https.onRequest(async (req, res) => {
+exports.getObject = functions.https.onRequest((req, res) => {
+  res.set('Access-Control-Allow-Origin', '*');
+  res.set('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  res.set('Access-Control-Allow-Headers', 'Content-Type');
+
   const objId = req.query.obj_id;
-  if (!objId) {
-    return res.status(400).send('obj_id is required');
-  }
-  const doc = await db.collection('objects').doc(objId).get();
-  if (!doc.exists) {
-    return res.status(404).send('No such document!');
-  }
-  res.status(200).json(doc.data());
+  admin.firestore().collection('objects').doc(objId).get()
+    .then(doc => {
+      if (!doc.exists) {
+        res.status(404).send('Object not found');
+      } else {
+        res.status(200).json(doc.data());
+      }
+    })
+    .catch(error => {
+      console.error('Error getting object:', error);
+      res.status(500).send('Error getting object');
+    });
 });
 
-exports.deleteObject = functions.https.onRequest(async (req, res) => {
+exports.deleteObject = functions.https.onRequest((req, res) => {
+  res.set('Access-Control-Allow-Origin', '*');
+  res.set('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  res.set('Access-Control-Allow-Headers', 'Content-Type');
+
   const objId = req.query.obj_id;
-  if (!objId) {
-    return res.status(400).send('obj_id is required');
-  }
-  await db.collection('objects').doc(objId).delete();
-  res.status(200).send('Object deleted');
+  admin.firestore().collection('objects').doc(objId).delete()
+    .then(() => {
+      res.status(200).send('Object deleted successfully');
+    })
+    .catch(error => {
+      console.error('Error deleting object:', error);
+      res.status(500).send('Error deleting object');
+    });
 });
 
-exports.getAllObjects = functions.https.onRequest(async (req, res) => {
-  const snapshot = await db.collection('objects').get();
-  let objects = [];
-  snapshot.forEach(doc => {
-    objects.push(doc.data());
-  });
-  res.status(200).json(objects);
+exports.getAllObjects = functions.https.onRequest((req, res) => {
+  res.set('Access-Control-Allow-Origin', '*');
+  res.set('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  res.set('Access-Control-Allow-Headers', 'Content-Type');
+
+  admin.firestore().collection('objects').get()
+    .then(snapshot => {
+      const objects = [];
+      snapshot.forEach(doc => {
+        objects.push(doc.data());
+      });
+      res.status(200).json(objects);
+    })
+    .catch(error => {
+      console.error('Error getting all objects:', error);
+      res.status(500).send('Error getting all objects');
+    });
 });
